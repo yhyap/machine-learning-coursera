1794e3db0c67346ccb73de554645da4cb5ed9881
Gourav Suri <gourav96suri@yahoo.com>
2018-01-02 20:40:16 +0530

    optimise to normalise for N features

--- mlclass-ex1/featureNormalize.m
+++ mlclass-ex1/featureNormalize.m
@@ -25,30 +25,11 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-mean_X1=mean(X(:,1));
-std_X1=std(X(:,1));
-mean_X2=mean(X(:,2));
-std_X2=std(X(:,2));
-k=size(X);
-m=k(1,1); 	
-
-%mu=[mean_X1(1:m,1), mean_X2(1:m,2)];
-for i=1:m
-mu(i,1)=mean_X1;
-mu(i,2)=mean_X2;
+for i=1:n 
+	mu(i) = mean(X(:,i));
+	sigma(i) = std(X(:,i));
+	X_norm(:,i) = (X(:,i) - mu(i))/sigma(i);
 end
-%sigma=[std_X1(1:m,1),std_X2(1:m,2)];
-for i=1:m
-sigma(i,1)=std_X1;
-sigma(i,2)=std_X2;
-end
-
-% Compute X_norm
-X_norm=(X-mu)./sigma
-
-
-
-
 
 
 
